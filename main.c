05f3fb120b2f7c623423f1cc9036b736100c77ee
victorlin7534 <vlin5@stuy.edu>
2018-11-02 11:47:34 -0400

    4

--- main.c
+++ main.c
@@ -4,20 +4,27 @@
 #include <dirent.h>
 #include <sys/types.h>
 
-int main(){
+int main(int argc, char *argv[]){
   DIR *directory;
   long size=0;
-  directory = opendir("./");
+  char *dir = "./";
+  if(argc > 1)
+    dir = argv[1];
+  directory = opendir(dir);
   struct dirent *cur;
-  struct stat *file;
+  struct stat file;
   while(cur = readdir(directory)){
-    printf("file: %s\n",cur->d_name);
+    printf("%s ",cur->d_name);
     if(cur->d_type==DT_REG){
-      stat(cur->d_name,file);
-      size += file->st_size;
+       stat(cur->d_name,&file);
+       printf("file %ld\n",file.st_size);
+       size += file.st_size;
+    }
+    else{
+      printf("directory\n");
     }
   }
-  printf("directory size: %ld",size);
+  printf("directory size: %ld\n",size);
   closedir(directory);
   return 0;
 }
